49e19526702913132e26aa59fd7400af5c027ec7
Andrew <ageng@wisc.edu>
2019-01-18 15:57:21 -0600

    Updated bookmarks page

--- client/src/pages/Bookmark.js
+++ client/src/pages/Bookmark.js
@@ -1,17 +1,61 @@
 import React, { Component } from "react";
+import {
+  Collapse,
+  Container,
+  Button,
+  ListGroup,
+  ListGroupItem
+} from "reactstrap";
 import Navbar from "../components/navbar";
-import { Container, Button } from "reactstrap";
+import BookmarkTab from "../components/bookmarkTab";
 
 class Bookmark extends Component {
-  //state = {  }
+  constructor(props) {
+    super(props);
+    this.collapseToggle = this.collapseToggle.bind(this);
+    this.popoverToggle = this.popoverToggle.bind(this);
+    this.tooltipToggle = this.tooltipToggle.bind(this);
+    this.state = { collapse: false, popoverOpen: false, tooltipOpen: false };
+  }
+
+  collapseToggle() {
+    this.setState({ collapse: !this.state.collapse });
+  }
+
+  popoverToggle() {
+    this.setState({ popoverOpen: !this.state.popoverOpen });
+  }
+
+  tooltipToggle() {
+    this.setState({ tooltipOpen: !this.state.tooltipOpen });
+  }
+
   render() {
     return (
       <div>
         <Navbar />
         <Container style={{ padding: "10px" }}>
-          <Button color="primary">
-            <a href="https://www.google.com">Google</a>
-          </Button>
+          <ListGroup>
+            <ListGroupItem>
+              <Button
+                style={{ width: "100%", marginBottom: "1rem" }}
+                color="primary"
+                onClick={this.collapseToggle}
+              >
+                Category 1
+              </Button>
+              <Collapse isOpen={this.state.collapse}>
+                <ListGroup>
+                  <ListGroupItem>
+                    <BookmarkTab />
+                  </ListGroupItem>
+                  <ListGroupItem>
+                    <BookmarkTab />
+                  </ListGroupItem>
+                </ListGroup>
+              </Collapse>
+            </ListGroupItem>
+          </ListGroup>
         </Container>
       </div>
     );

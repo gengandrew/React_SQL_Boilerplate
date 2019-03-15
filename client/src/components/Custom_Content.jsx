a0e045b82476d1f229ebb5a21c37c08ac6477318
Andrew Geng <gengandrew88@gmail.com>
2019-03-15 14:01:38 -0500

    updated comments

--- client/src/components/Custom_Content.jsx
+++ client/src/components/Custom_Content.jsx
@@ -9,7 +9,7 @@ class Custom_Content extends Component {
   render() {
     return (
       <div>
-        <MainNavbar />
+        <MainNavbar/>
         <Button color="primary" href="http://localhost:5000/api/getAll" style={{marginLeft: "10px"}}>Widget</Button>
         <Button color="primary" href="http://localhost:3000/Bookmark" style={{marginLeft: "10px"}}>Bookmark</Button>
       </div>
